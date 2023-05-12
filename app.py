c3f0a8b876383de350eaee5ff8395552082934fe
Sai-1323 <111491949+Sai-1323@users.noreply.github.com>
2023-05-12 16:09:20 +0000

    Created jobs section

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+Jobs = [
+    {
+        'id' : 1,
+        'title' : 'Data Analyst',
+        'location' : 'Hyderabad, India',
+        'salary' : 'Rs. 15,00,000'
+    },
+    {
+        'id' : 2,
+        'title' : 'Full Stack',
+        'location' : 'Kolkata, India',
+        'salary' : 'Rs. 25,00,000'
+    },
+    {
+        'id' : 3,
+        'title' : 'Java Dev',
+        'location' : 'Delhi, India',
+        'salary' : 'Rs. 45,00,000'
+    },
+    {
+        'id' : 4,
+        'title' : 'AI Dev',
+        'location' : 'New York, U.S.A',
+        'salary' : '$ 1,00,000'
+    }
+]
 
 # route is part of url after the domain name
 # ex : in goggle.com/search , "/search" is route
 # the below is aka empty route that means the it is the home page
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs = Jobs,company="PSD Industries")
 
 
 if __name__ == "__main__":
