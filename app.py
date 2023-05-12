583314a64937c14a17aa2596fff9f33f4a62fa4e
Sai-1323 <111491949+Sai-1323@users.noreply.github.com>
2023-05-12 09:09:59 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
-# route is part of url after the domain name 
+
+
+# route is part of url after the domain name
 # ex : in goggle.com/search , "/search" is route
 # the below is aka empty route that means the it is the home page
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-    
+    return render_template('home.html')
+
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0' , debug = True)
-
-
+    app.run(host='0.0.0.0', debug=True)
