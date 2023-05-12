a4f9b5c5c037b61cc242415a32ece9ac63b4fe4d
Sai-1323 <111491949+Sai-1323@users.noreply.github.com>
2023-05-12 08:08:01 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+# route is part of url after the domain name 
+# ex : in goggle.com/search , "/search" is route
+# the below is aka empty route that means the it is the home page
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+    
 
-'''if __name__ == "__main__":
-  app.run("0.0.0.0",)'''
+if __name__ == "__main__":
+  app.run(host = '0.0.0.0' , debug = True)
 
-print("Hello Jello")
+
